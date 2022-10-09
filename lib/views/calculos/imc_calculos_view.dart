276d187e36fe694e33fe03adeec79d9fcca2e09a
Sergiok12 <sergioabd460@gmail.com>
2022-10-09 13:38:51 +0200

    Arrgelado, funcion basica completa

--- lib/views/calculos/imc_calculos_view.dart
+++ lib/views/calculos/imc_calculos_view.dart
@@ -2,6 +2,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 // ignore: must_be_immutable
 class IndiceDemasaCorporal extends StatefulWidget {
@@ -22,9 +23,39 @@ class _IndiceDemasaCorporalState extends State<IndiceDemasaCorporal> {
   final myControlleraltura = TextEditingController();
 
   @override
-  Widget build(BuildContext context) { 
+  Widget build(BuildContext context) {
+    void calcxd(double pes) {
+      if (resultado.toString().isEmpty || resultado == 0.00) {
+        resultado = 0.00;
+        elc = Colors.blue;
+        estado = '';
+      } else {
+        if (resultado <= 18.40 && resultado > 0.00) {
+          estado = "Desnuticion";
+          elc = Colors.yellow;
+        } else if (resultado >= 18.5 && resultado <= 24.999) {
+          estado = "Normal";
+          elc = Colors.green;
+        } else if (resultado >= 25 && resultado <= 29.999) {
+          estado = "Sobrepeso";
+          elc = Colors.orange;
+        } else if (resultado >= 30 && resultado <= 34.999) {
+          estado = "Obesidad grado 1";
+          elc = Colors.red.shade300;
+        } else if (resultado >= 35 && resultado <= 39.999) {
+          estado = "Obesidad grado 2";
+          elc = Colors.red;
+        } else if (resultado > 40 && resultado <= 100) {
+          estado = "Obesidad grado 3";
+          elc = Colors.red.shade700;
+        } else {
+          estado = "Ha ocurrido un error";
+          elc = Colors.grey;
+        }
+      }
+    }
 
-    resultado = calcular( peso, altura );  
+    resultado = calcular(peso, altura);
 
     return Scaffold(
       appBar: AppBar(
@@ -35,104 +66,32 @@ class _IndiceDemasaCorporalState extends State<IndiceDemasaCorporal> {
           children: [
             Row(
               children: [
-                const Text("Ingrese el peso"),
+                const Text("Ingrese el peso(Kg)"),
                 Expanded(
-                  child: TextField(
-                    textAlign: TextAlign.center,
-                    onChanged: (value) {
-                      if (value.trim.toString().isEmpty == true) {
-                      resultado = 0.00;
-                      elc =  Colors.blue;
-                      estado = '';
-                      }else{
-                        setState(() {
-                        peso = double.tryParse(myControllerpeso.text) ?? 0.00;
-
-                        if(resultado == 0.00){
-                          estado = '';
-                          elc = Colors.blue;
-                        }
-                        else if(resultado <= 18.40 && resultado > 0.00){
-                          estado = "Desnuticion";
-                          elc = Colors.yellow;
-                        }
-                        else if(resultado >= 18.5 && resultado <=24.999){
-                          estado = "Normal";
-                          elc = Colors.green;
-                        }
-                        else if(resultado >= 25 && resultado <= 29.999){
-                          estado = "Sobrepeso";
-                          elc = Colors.orange;
-                        }
-                        else if(resultado >= 30 && resultado <= 34.999){
-                          estado = "Obesidad grado 1";
-                          elc = Colors.red.shade300;
-                        }
-                        else if(resultado >= 35 && resultado<= 39.999){
-                          estado = "Obesidad grado 2";
-                          elc = Colors.red;
-                        }
-                        else if(resultado > 40 && resultado <=100){
-                          estado = "Obesidad grado 3";
-                          elc = Colors.red.shade700;
-                        }
-                        else{
-                          estado = "Ha ocurrido un error";
-                          elc = Colors.grey;
-                        }
-                      }
-                    );
-                    }
+                    child: TextField(
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    setState(() {
+                      peso = double.tryParse(myControllerpeso.text) ?? 0.00;
+                      resultado = calcular(peso, altura);
+                      calcxd(resultado);
+                    });
                   },
                   controller: myControllerpeso,
                   keyboardType: TextInputType.number,
                 )),
-                const Text("Ingrese la altura"),
+                const Text("Ingrese la altura(M)"),
                 Expanded(
                     child: TextField(
-                      textAlign: TextAlign.center,
-                      onChanged: (value) {
-                        if (value.trim.toString().isEmpty == true) {
-                          resultado = 0.00;
-                          elc =  Colors.blue;
-                          estado = '';
-                        } else {
-                          setState(() {
-                            altura = double.tryParse(myControlleraltura.text) ?? 0.00;                           
-                        if(resultado == 0.00){
-                          estado = '';
-                          elc = Colors.blue;
-                        }
-                        else if(resultado <= 18.40 && resultado > 0.00){
-                          estado = "Desnuticion";
-                          elc = Colors.yellow;
-                        }
-                        else if(resultado >= 18.5 && resultado <=24.99){
-                          estado = "Normal";
-                          elc = Colors.green;
-                        }
-                        else if(resultado >= 25 && resultado <= 29.999){
-                          estado = "Sobrepeso";
-                          elc = Colors.orange;
-                        }
-                        else if(resultado >= 30 && resultado <= 34.999){
-                          estado = "Obesidad grado 1";
-                          elc = Colors.red.shade300;
-                        }
-                        else if(resultado >= 35 && resultado<= 39.999){
-                          estado = "Obesidad grado 2";
-                          elc = Colors.red;
-                        }
-                        else if(resultado > 40 && resultado <=100){
-                          estado = "Obesidad grado 3";
-                          elc = Colors.red.shade700;
-                        }
-                        else{
-                          estado = "Ha ocurrido un error";
-                          elc = Colors.grey;
-                        }
-                      });
-                    }
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    textAlign: TextAlign.center,
+                    onChanged: (value) {
+                    setState(() {
+                      altura = double.tryParse(myControlleraltura.text) ?? 0.00;
+                      resultado = calcular(peso, altura);
+                      calcxd(resultado);
+                    });
                   },
                   controller: myControlleraltura,
                   keyboardType: TextInputType.number,
@@ -143,15 +102,13 @@ class _IndiceDemasaCorporalState extends State<IndiceDemasaCorporal> {
             Row(
               children: [
                 Expanded(
-                  child: Container(
-                    height: 50,
-                    color: elc,
-                    child: Text(
-                      "${resultado.toStringAsFixed(2)}\n $estado",
-                      textAlign: TextAlign.center,
-                    )
-                  )
-                )
+                    child: Container(
+                        height: 50,
+                        color: elc,
+                        child: Text(
+                          "${resultado.toStringAsFixed(2)}\n $estado",
+                          textAlign: TextAlign.center,
+                        )))
               ],
             )
           ],
@@ -159,19 +116,16 @@ class _IndiceDemasaCorporalState extends State<IndiceDemasaCorporal> {
       ),
     );
   }
-  
-  double calcular(double pesox,double alturax) {
-    if(alturax == 0 && pesox == 0){
+
+  double calcular(double pesox, double alturax) {
+    if (alturax == 0 && pesox == 0) {
       return 0.00;
     }
-     if(alturax == 0 || pesox == 0){
+    if (alturax == 0 || pesox == 0) {
       return 0.00;
     }
-    if(alturax == 0 && peso != 0){
-      return 0.00;
-    }
-    double resx = (pesox /(pow(alturax,2)));
+
+    double resx = (pesox / (pow(alturax, 2)));
     return resx;
   }
-  
 }
