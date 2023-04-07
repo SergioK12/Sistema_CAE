2752fcba0739877be9523f0775893d5238221b30
Sergiok12 <sergioabd460@gmail.com>
2023-04-06 22:02:31 -0600

    Modo oscuro o claro con persistencia

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,38 @@
+import 'package:cae/settings/setting_services.dart';
+import 'package:cae/settings/settings_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'generated/l10n.dart';
 import 'ui/logic/main_provider.dart';
-import 'ui/styles/themes.dart';
 import 'ui/views/home_view.dart';
 
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() => runApp(const MyApp(),
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+await Preferencias.init();
+runApp(
+  MultiProvider(providers: [
+    ChangeNotifierProvider(create: (context)=> ThemeProvider(isDarkMode: Preferencias.getIsDarkMode),
+    )
+  ],child: const MyApp(),)
 );
+}
+
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
+
      return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => Mainprovider()),
-        
       ],
       child: MaterialApp(
       title: 'Sistema CAE',
-      theme: themedatalight,
+      theme: Provider.of<ThemeProvider>(context).temaactual,
       initialRoute: HomeView.routeName,
       routes: {
         HomeView.routeName: (_) => const HomeView(),
