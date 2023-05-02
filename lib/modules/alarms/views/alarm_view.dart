9aa3fd249c5324613ada3bef762a1a58c69bc202
Sergiok12 <sergioabd460@gmail.com>
2023-05-01 20:16:45 -0600

    Prueba inicial

--- lib/modules/alarms/views/alarm_view.dart
+++ lib/modules/alarms/views/alarm_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_alarm_clock/flutter_alarm_clock.dart';
 
 class AlarmsView extends StatefulWidget {
   const AlarmsView({super.key});
@@ -10,8 +11,17 @@ class AlarmsView extends StatefulWidget {
 class _AlarmsViewState extends State<AlarmsView> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Alarma"),
+    return  Center(
+      child: Column(
+        children: [
+          ElevatedButton(
+            onPressed: (){
+              FlutterAlarmClock.createAlarm(20, 16);
+
+            }, 
+            child: const Text("Presiona"))
+        ],
+      ),
     );
   }
 }
