01ea87d64589cccd32e37d19cd327a3d25d58bae
Sergiok12 <sergioabd460@gmail.com>
2023-05-04 20:41:49 -0600

    Paginas de calculos agregadas

---
+++ lib/modules/calcules/views/IMC/indice_masa.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class IMCView extends StatelessWidget {
+  const IMCView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(title: const Text("Indice de masa corporal"),),
+    );
+  }
+}
