86e0ec7e7757c2ecb7d292e0742fd0196c62f803
Sergiok12 <sergioabd460@gmail.com>
2023-05-11 21:33:01 -0600

    Configuracion primitiva de la vista

--- lib/modules/calcules/views/IMC/indice_masa.dart
+++ lib/modules/calcules/views/IMC/indice_masa.dart
@@ -14,25 +14,86 @@ class IMCView extends StatelessWidget {
         children: [
           Container(
             padding: const  EdgeInsets.all(20),
+            
             color: Colors.yellow,
             width: double.infinity,
             child: const Text("Calcular", textAlign: TextAlign.center),
           ),
-          const Padding(
-            padding:  EdgeInsets.symmetric(horizontal: 20),
-            child:  TextField(
-              decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20))
-                )
-              ),
+          
+          Padding(
+            padding: const EdgeInsets.only(top: 20, left: 20),
+            child: Row(
+              children:  [
+                 const Padding(
+                   padding:  EdgeInsets.only(right: 20),
+                   child: Text("Peso"),
+                 ),
+                 const Expanded(
+                  flex: 2,
+                   child: TextField(
+                     decoration: InputDecoration(
+                       border: OutlineInputBorder(
+                         borderRadius: BorderRadius.all(Radius.circular(20))
+                       ),
+                     ),
+                   ),
+                 ),
+                 Row(
+                   children: [
+                    const Padding(
+                      padding:  EdgeInsets.only(left: 20),
+                      child:  Text("Libras"),
+                    ),
+                     Switch(value: true,
+                      onChanged: (value) {
+                       debugPrint(value.toString());
+                     }, ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20, ),
+                      child:  Text("KG")),
+
+                   ],
+                 ),
+              ],
             ),
           ),
-          const Padding(
-            padding:  EdgeInsets.symmetric(horizontal: 20),
-            child:  TextField(),
+          Padding(
+            padding: const EdgeInsets.only(top: 20, left: 20),
+            child: Row(
+              children:  [
+                 const Padding(
+                   padding:  EdgeInsets.only(right: 20),
+                   child: Text("Altura"),
+                 ),
+                 const Expanded(
+                  flex: 2,
+                   child: TextField(
+                     decoration: InputDecoration(
+                       border: OutlineInputBorder(
+                         borderRadius: BorderRadius.all(Radius.circular(20))
+                       ),
+                     ),
+                   ),
+                 ),
+                 Row(
+                   children: [
+                    const Padding(
+                      padding:  EdgeInsets.only(left: 20),
+                      child:  Text("Metros"),
+                    ),
+                     Switch(value: true,
+                      onChanged: (value) {
+                       debugPrint(value.toString());
+                     }, ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20, ),
+                      child:  Text("CM")),
+
+                   ],
+                 ),
+              ],
+            ),
           ),
-          
 
         ],
 
